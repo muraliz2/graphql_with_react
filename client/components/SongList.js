1ebebc3d5db387099d7058b74a4b687630102f30
Murali Elumalai <murali@sribarati.com>
2018-11-29 17:53:23 +0530

    add(SongList): songlist component

---
+++ client/components/SongList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class SongList extends Component {
+  render() {
+    return (
+      <div>
+        <p>SongList</p>
+      </div>
+    )
+  }
+}
+export default SongList
--- client/index.js
+++ client/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ApolloClient } from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
-
+import SongList from './components/SongList'
 const client = new ApolloClient({})
 
 const Root = () => {
   return (
-    // <ApolloProvider client={client}>
-    <div>Lyrical</div>
-    // </ApolloProvider>
+    <ApolloProvider client={client}>
+      <SongList />
+    </ApolloProvider>
   )
 }
 
